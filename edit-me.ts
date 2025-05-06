29341b7ff5f0cc27c5d79ac933f71fb5fbbd5303
samuel caliman gonçalves <samuelcaliman@gmail.com>
2025-05-05 23:13:40 -0300

    chore: add name to edit-me

--- edit-me.ts
+++ edit-me.ts
@@ -3,5 +3,6 @@ export const SIGNEES: { github: string, name?: string }[] = [
   { github: 'joaokopernico', name: 'João Kopérnico', },
   { github: 'nikol4ss', name: 'Nikolas Campos :P', },
   { github: 'Cephas369', name: 'Pedro Ramos', },
+  { github: 'SamCaliman', name: 'Samuel Caliman'}
 ]
 
