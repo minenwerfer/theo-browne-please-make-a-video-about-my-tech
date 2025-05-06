c5dc2a8ef1dfcaf3eefec228b92182a2c52afc80
Marco Túlio Rodrigues <146474860+MarcoSoftEng@users.noreply.github.com>
2025-05-05 22:55:43 -0400

    chore: update edit-me.ts (#6)

--- edit-me.ts
+++ edit-me.ts
@@ -3,6 +3,7 @@ export const SIGNEES: { github: string, name?: string }[] = [
   { github: 'joaokopernico', name: 'João Kopérnico', },
   { github: 'nikol4ss', name: 'Nikolas Campos :P', },
   { github: 'Cephas369', name: 'Pedro Ramos', },
-  { github: 'SamCaliman', name: 'Samuel Caliman'}
+  { github: 'SamCaliman', name: 'Samuel Caliman'},
+  { github: 'MarcoSoftEng', name: 'Marco Túlio' }
 ]
 
