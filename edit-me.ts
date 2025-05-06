286b1c3066e5e151ee431d245a139430c90bdc75
Cephas369 <84059136+Cephas369@users.noreply.github.com>
2025-05-05 21:13:31 -0400

    Update edit-me.ts

--- edit-me.ts
+++ edit-me.ts
@@ -2,5 +2,6 @@ export const SIGNEES: { github: string, name?: string }[] = [
   { github: 'minenwerfer', name: 'João Santos', },
   { github: 'joaokopernico', name: 'João Kopérnico', },
   { github: 'nikol4ss', name: 'Nikolas Campos :P', },
+  { github: 'Cephas369', name: 'Pedro Ramos', },
 ]
 
